b3a39b34215cdded63985cd7d8c2e2aeeda39556
Jacob LeCoq <lecoqjacob@gmail.com>
2023-07-19 20:54:14 -0500

    oh a human

--- bin/common.ml
+++
@@ -1,6 +0,0 @@
-(* Location *)
-type loc = int * int
-
-(* Regions *)
-type region_id = int
-type region_loc = int * loc
--- bin/view.ml
+++
@@ -1,8 +0,0 @@
-let draw_scene draw_func =
-  let open Raylib in
-  begin_drawing ();
-  clear_background Color.raywhite;
-
-  draw_func ();
-
-  end_drawing ()
